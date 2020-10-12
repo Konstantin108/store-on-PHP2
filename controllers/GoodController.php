305bfdf06a108dd15ff03545c96e4021af1271fa
Kostya <Kpstya@mail.ru>
2020-10-13 00:27:58 +0300

    for switch

--- controllers/GoodController.php
+++ controllers/GoodController.php
@@ -44,20 +44,38 @@ class GoodController extends Controller
             $id = $_POST['idForUpdate'];
             $name = $_POST['nameForUpdate'];
             $price = $_POST['priceForUpdate'];
+            $material = $_POST['materialForUpdate'];
+            $designer = $_POST['designerForUpdate'];
+            $size = $_POST['sizeForUpdate'];
+            $color = $_POST['colorForUpdate'];
+            $sex = $_POST['sexForUpdate'];
             $info = $_POST['infoForUpdate'];
-
-            $counter = 1;
+            $img = "/img/unnamed.jpg";
 
             $good = new Good();
             $good->id = $id;
             $good->name = $name;
             $good->price = $price;
+            $good->material = $material;
+            $good->designer = $designer;
+            $good->size = $size;
+            $good->color = $color;
+            $good->sex = $sex;
             $good->info = $info;
-            $good->counter = $counter;
+            $good->img = $img;
 
-            if(!empty($name) && !empty($price) && !empty($info) && !empty($counter)){
+            if(!empty($name) &&
+            !empty($price) &&
+            !empty($material) &&
+            !empty($designer) &&
+            !empty($size) &&
+            !empty($color) &&
+            !empty($sex) &&
+            !empty($info) &&
+            !empty($img)
+                ){
                 $this->container->goodRepository->save($good);
-                header('Location: /good/all');   //<--путь изменён для twig
+                header('Location: /good');   //<--путь изменён для twig
                 return '';
 
             }else{
@@ -82,7 +100,7 @@ class GoodController extends Controller
             $good = new Good();
             $good->id = $id;
             $this->container->goodRepository->delete($good);
-            header('Location: /good/all');   //<--путь изменён для twig
+            header('Location: /good');   //<--путь изменён для twig
             return '';
     }
 
